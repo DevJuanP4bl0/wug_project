0f6c06570b1eb46cea73d98f68b1a75d6f4cfb67
Juan <juanpablo140204@outlook.com>
2026-04-03 16:36:03 -0300

    adding the function genome2features

--- wug.c
+++ wug.c
@@ -12,3 +12,24 @@ wug_t create_wug(int genome[16], gender_t g){
 
     return wug;
 }
+
+void genome2features(const int genome[16], int features[4]){
+    int super[] = {1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0};
+    int zones[] = {0, 1, 0, 2, 3, 3, 1, 0, 2, 1, 1, 2, 3, 0, 3, 2};
+
+    int count[] = {0, 0, 0, 0};
+
+    for (int i = 0; i < 16; i++){
+        if (genome[i] == super[i]){
+            int zone = zones[i];
+            count[zone]++;
+        }
+    }
+
+    for (int i = 0; i < 4; i++){
+        if (count[i] == 4)
+            features[i] = 1;
+        else
+            features[i] = 0;
+    }
+}
